d885d263cf590d175733b56574e9cb31f408a720
Maruv-git <72843158+Maruv-git@users.noreply.github.com>
2020-10-23 13:38:25 -0700

    Update script.py

--- script.py
+++ script.py
@@ -1,19 +1,27 @@
-from Bio import SeqIO
-from Bio.SeqFeature import FeatureLocation, SeqFeature
+ffrom Bio import SeqIO
 from Bio.SeqRecord import SeqRecord
 import os
-filename= "/home/Maruv-git/Ejercicio-de-Biopython"
+
+# Cambiar la dirección del archivo .gbk que desee leer
+filename =  "/mnt/c/Users/MUDTL/Docuemnts/git/Ejercicio-biopython/ls_orchid.gbk"
+
 def summarize_contents(filename):
-        all_records=[]
-        records = list(SeqIO.parse(filename, "genbank"))
-        print ("Path: ", os.path.dirname(filename))
-        print("num_records = %i records" % len(records))
-        print("\n\n")
-        for seq_record in SeqIO.parse(filename, "genbank"):
-                all_records.append(seq_record.name)
-                print(counter, ".-")
-                print("Name: ", seq_record.name)
-                print("ID:",seq_record.id)
-                print("Description: ", seq_record.description)
-                
-summarize_contents(filename)
+	ListaR = []
+	ListaR = os.path.split(filename)
+	Cadena = " "
+	Cadena = ("file: "+ ListaR[1] + "\npath: " + ListaR[0])
+	# Número de registros
+	all_records=[]
+	records = list(SeqIO.parse(filename, "genbank"))
+	Cadena += ("\nnum_records: " + str(len(records)))
+	Cadena += ("\nrecords:")
+	# Registros
+	for seq_record in SeqIO.parse(filename, "genbank"):
+		all_records.append(seq_record.name)
+		Cadena += ("\n- id:" + str(seq_record.id))
+		Cadena += ("\nname: " + seq_record.name)
+		Cadena += ("\ndescription: " + str(seq_record.description))
+		Cadena += ("\n")
+	return Cadena
+R = summarize_contents(filename)
+print(R)
