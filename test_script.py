359bf343b9a7dc7340f90ea7d977dad39877e0af
Maruv-git <72843158+Maruv-git@users.noreply.github.com>
2020-11-08 19:30:27 -0700

    biopython-tarea2-ex2]
    Antes, la prueba estaba en un archivo diferente, se cambió de lugar

--- test_script.py
+++ test_script.py
@@ -4,7 +4,6 @@ import script
 class prueba(unittest.TestCase):
   def test_summarize_contents(self):
     
-
       s = summarize_contents("data/AF323668.gbk")
       self.assertEqual("file: AF323668.gbk\npath: C:\Users\MUDTL\Documents\GitHub\Ejercicio-biopython\data\AF323668.gbk")
 
@@ -23,6 +22,29 @@ class prueba(unittest.TestCase):
       s = summarize_contents("data/opuntia.fasta")
       self.assertEqual("file: opuntia.fasta\npath: C:\Users\MUDTL\Documents\GitHub\Ejercicio-biopython\data\opuntia.fasta" )
 
+ #prueba de concatenación y complemento //////////////////////////////////////////////////////////////////////////////////////////////     
+      
+class prueban2(unittest.TestCase):
+  def test_concatenate_get_reverse_of_complement(self):
+    
+      Ej1 = Sec("ATAGACATAGAAATAGATACATAGAT")
+      Cadrev = concatenate_and_get_reverse_of_complement("ATCGATAGCTAGATCGATATAG", "TCAGATAGCGGGTACAGTGTGTT")
+      self.assertEqual(Ej1,Cadrev)
 
+      Ej2 = Sec("ATACTAGATAGATGGGGATCACTGAC")
+      Cadrev = concatenate_and_get_reverse_of_complement("atgacagatagacacaggctcgatgatcggtgatc", "ggtagacagatttgatcggta")
+      self. assertEqual(Ej2,Cadrev)
+
+      Ej3 = Sec("ATCGCTAGTagatcTgTAGAtGtG")
+      Cadrev = concatenate_and_get_reverse_of_complement("ATGCGATgTCgGtaATg", "AAATgCcGATAGtGcGtG")
+      self.assertEqual(Ej3,Cadrev)
+
+      Ej4 = Sec("AGGCTCGTGATAgTgTgTgAAcGT")
+      Cadrev = concatenate_and_get_reverse_of_complement("cagatagacgtcgctcgctgggaagtcgt", "gatagatacacagatagaagata")
+      self.assertEqual(Ej4,Cadrev)
+
+      Ej5 = Sec("agatcagatctgcgctgagagcccacagagagtatttttt")
+      Cadrev = concatenate_and_get_reverse_of_complement("CGATAGCTGTAGCGTAGCGTA", "gtcgtgtcgctagcgatcgataaa")
+      self.assertEqual(Ej5,Cadrev)
 
 
